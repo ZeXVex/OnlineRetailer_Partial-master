40bbdff8f17492ba832fbf0f60d6117555b4a333
Nicolai <nicolaiwork@live.dk>
2020-03-18 13:47:40 +0100

    fix1
    fixed the in memory by still not the config

--- CustomerApi/Startup.cs
+++ CustomerApi/Startup.cs
@@ -5,6 +5,7 @@ using CustomerApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
